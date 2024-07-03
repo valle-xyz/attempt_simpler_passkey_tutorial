39fe578aa06d02079452b66c5039c50515631564
valle-xyz <valentin.seehausen@gmail.com>
2024-07-03 19:32:16 +0200

    add Safe address

--- app/page.tsx
+++ app/page.tsx
@@ -6,16 +6,41 @@ import loadPassKey from "@/lib/loadPasskey";
 import storePassKey from "@/lib/storePasskey";
 import { PasskeyArgType } from "@safe-global/protocol-kit";
 import { useEffect, useState } from "react";
+import { Safe4337Pack } from "@safe-global/relay-kit";
+import {
+  RPC_URL,
+  BUNDLER_URL,
+  CHAIN_NAME,
+  PAYMASTER_URL,
+  paymasterAddress,
+} from "../lib/constants";
 
 export default function Home() {
   const [selectedPasskey, setSelectedPasskey] = useState<PasskeyArgType>();
+  const [safeAddress, setSafeAddress] = useState<string>();
 
   useEffect(() => {
     const passkey = loadPassKey();
     if (passkey) {
-      setSelectedPasskey(passkey);
+      handleSelectPasskey(passkey);
     }
   }, []);
+
+  const handleSelectPasskey = async (passkey: PasskeyArgType) => {
+    setSelectedPasskey(passkey);
+
+    const safe4337Pack = await Safe4337Pack.init({
+      provider: RPC_URL,
+      signer: passkey,
+      bundlerUrl: BUNDLER_URL,
+      options: {
+        owners: [],
+        threshold: 1,
+      },
+    });
+
+    setSafeAddress(await safe4337Pack.protocolKit.getAddress());
+  };
 
   const handleCreatePasskey = async () => {
     const passkey = await createPassKey();
@@ -28,9 +53,14 @@ export default function Home() {
       <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm ">
         <p>Hello</p>
         {selectedPasskey ? (
-          <div className="flex flex-col items-center justify-between">
-            <p>Passkey: {selectedPasskey.rawId}</p>
-          </div>
+          <>
+            <div className="flex flex-col items-center justify-between">
+              <p>Passkey: {selectedPasskey.rawId}</p>
+            </div>
+            <div className="flex flex-col items-center justify-between">
+              <p>Safe Address: {safeAddress}</p>
+            </div>
+          </>
         ) : (
           <button
             onClick={handleCreatePasskey}
--- lib/constants.ts
+++ lib/constants.ts
@@ -1,1 +1,31 @@
 export const STORAGE_PASSKEY_LIST_KEY = "__PASSKEY_SAFE_DEMO_APP__";
+
+// RPC URL
+// export const RPC_URL = "https://sepolia.gateway.tenderly.co"
+export const RPC_URL = "https://ethereum-sepolia-rpc.publicnode.com";
+// export const RPC_URL = "https://rpc.ankr.com/eth_sepolia"; // SEPOLIA
+// export const RPC_URL = 'https://rpc.gnosischain.com/' // GNOSIS
+
+// CHAIN
+export const CHAIN_NAME = "sepolia";
+// export const CHAIN_NAME = 'gnosis'
+
+// USDC CONTRACT ADDRESS IN SEPOLIA
+// faucet: https://faucet.circle.com/
+export const usdcTokenAddress = "0x1c7D4B196Cb0C7B01d743Fbc6116a902379C7238"; // SEPOLIA
+// const usdcTokenAddress = '0xddafbb505ad214d7b80b1f830fccc89b60fb7a83' // GNOSIS
+
+const PIMLICO_API_KEY = "a199e8bb-e271-4a7d-8c0b-affe6632e308";
+
+// PAYMASTER ADDRESS
+export const paymasterAddress = "0x0000000000325602a77416A16136FDafd04b299f"; // SEPOLIA
+
+// Bundler URL
+export const BUNDLER_URL = `https://api.pimlico.io/v1/${CHAIN_NAME}/rpc?apikey=${PIMLICO_API_KEY}`; // PIMLICO
+
+// Paymaster URL
+export const PAYMASTER_URL = `https://api.pimlico.io/v2/${CHAIN_NAME}/rpc?apikey=${PIMLICO_API_KEY}`; // PIMLICO
+
+// NFT Contract address
+export const XANDER_BLAZE_NFT_ADDRESS =
+  "0xBb9ebb7b8Ee75CDBf64e5cE124731A89c2BC4A07";
